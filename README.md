7aa58410dbe758543cf25d62ff9d3ab88b9ace94
xiechufeng1 <974470833@qq.com>
2020-08-10 17:55:15 +0800

    xxx

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # small-config
-获取全局配置参数
-需运行的时候在全局设置 NODE_ENV
-small-config会根据NODE_ENV的值去配置目录查找对应的文件
+
+###获取全局配置参数
+
+####需运行的时候在全局设置 NODE_ENV
+####small-config会根据NODE_ENV的值去配置目录查找对应的文件
 
 
-
+```angular2
 使用方式，在入口文件导入
 import config from "small-config";
-
 config.load("配置文件目录");
 
+配置文件目录下面的文件会合并，并且必须要用一个env文件夹
+```
 
 
-其文件使用
+
+```angular2
+其他文件使用
 import config from "small-config";
 
-config.get("name")
+config.get("session.name")
+```
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-
-let config;
+const fs = require("fs");
+const path = require("path");
+let config={};
 const env = process.env.NODE_ENV;
 class smallConfig {
     static load(path){
-        path = path[path.length-1] !== '/' ? path+'/' : path;
-        config=require(path + env.trim() + '.js');
-        config=config.default?config.default:config;
+        path = path[path.length-1] !== '\\' ? path+'\\' : path;
+        console.log(path,"======");
+        fs.readdirSync(path).forEach(file =>{   //将目录下的js配置文件合并
+            if(file.indexOf(".")!==-1){
+                let obj = require(path + file.trim());
+                obj = obj.default ? obj.default : obj;
+                config = Object.assign({},config,obj);
+            }
+        })
+        let assign = require(`${path}env/${env.trim()}.js`);
+        assign=assign.default?assign.default:assign;
+        config = Object.assign({},config,assign||{});
     }
     static get(str){
-        return config[str] || null;
+        return new Function('config',`with(config){return ${str} }`)(config) || null;
     }
 }
+
 
 module.exports=smallConfig;
